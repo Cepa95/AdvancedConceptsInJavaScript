f7c81fc8c593cb1da6ea259b756222e0a3386502
Cepa95 <ceprnic.josip@gmail.com>
2023-09-11 11:06:43 +0200

    arguments Keyword

--- part01/app.js
+++ part01/app.js
@@ -39,7 +39,18 @@ india();
 
 function marry(person1, person2){
   console.log('arguments: ', arguments)
+  console.log(Array.from(arguments))
   return `${person1} is now married to ${person2}`;
+}
+
+console.log(marry ('Tim', 'Tina'))
+
+// arguments keyword
+
+function marry(...args){
+  console.log('arguments: ', args)
+  console.log(Array.from(arguments))
+  return `${args[0]} is now married to ${args[1]}`;
 }
 
 console.log(marry ('Tim', 'Tina'))
