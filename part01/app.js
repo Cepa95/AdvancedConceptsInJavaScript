d9876d82f1b8c263856dffc23ca7777ebbe0519d
Cepa95 <ceprnic.josip@gmail.com>
2023-09-11 10:55:14 +0200

    invocation

--- part01/app.js
+++ part01/app.js
@@ -3,7 +3,6 @@
 //HOISTING
 var favouriteFood = "grapes";
 
-
 var foodThoughts = function () {
   console.log("original favourite food: " + favouriteFood);
 
@@ -15,11 +14,32 @@ var foodThoughts = function () {
 foodThoughts();
 
 // partially hoisted variables
-console.log(sing())
-console.log(teddy)
-var teddy = 'bear'
+console.log(sing());
+console.log(teddy);
+var teddy = "bear";
 
 //undefined functions are fully hoisted
-function sing(){
-    console.log('la la la')
+function sing() {
+  console.log("la la la");
 }
+
+//function expression
+var canada = function () {
+  console.log("cold");
+};
+//function declaration
+
+function india() {
+  console.log("warm");
+}
+//invocation, call, execution
+canada();
+india();
+
+
+function marry(person1, person2){
+  console.log('arguments: ', arguments)
+  return `${person1} is now married to ${person2}`;
+}
+
+console.log(marry ('Tim', 'Tina'))
