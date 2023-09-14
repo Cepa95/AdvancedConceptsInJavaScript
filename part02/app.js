548a864da1175720495dfd77632f4af9039226d3
Cepa95 <ceprnic.josip@gmail.com>
2023-09-14 08:57:56 +0200

    js types

---
+++ part02/app.js
@@ -0,0 +1,22 @@
+// alert("Provjera");
+//PRIMITIVE=> sing value
+console.log(typeof 5)
+// true
+// 'To be or no to be'
+// undefined
+console.log(typeof null)
+console.log(typeof Symbol('just me'))
+
+
+function a(){
+    return 5
+}
+
+a.hi= 'Hello'
+
+console.log(a.hi)
+
+//NON-PRIMITIVE
+// {}
+// []
+// function(){}
