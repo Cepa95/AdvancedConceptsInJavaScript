6aaee9a1e969efdb47a55753a8c0c44f10e574e5
Cepa95 <ceprnic.josip@gmail.com>
2023-09-22 00:15:37 +0200

    OOP

--- part04/app.js
+++ part04/app.js
@@ -24,49 +24,64 @@
 
 // Object.create()
 
-const elfFunctions = {
-  attack() {
-    return "attacks with " + this.weapon;
-  },
-};
+// const elfFunctions = {
+//   attack() {
+//     return "attacks with " + this.weapon;
+//   },
+// };
 
-function createElf(name, weapon) {
-  let newElf = Object.create(elfFunctions);
-  newElf.name = name;
-  newElf.weapon = weapon;
-  return newElf;
-}
-const sam = createElf("sam", "fire");
-sam.attack = elfFunctions.attack;
-console.log(sam.attack());
+// function createElf(name, weapon) {
+//   let newElf = Object.create(elfFunctions);
+//   newElf.name = name;
+//   newElf.weapon = weapon;
+//   return newElf;
+// }
+// const sam = createElf("sam", "fire");
+// sam.attack = elfFunctions.attack;
+// console.log(sam.attack());
 
-//constructor functions
-function Elf(name, weapon) {
-  console.log("this:", this);
-  this.name = name;
-  this.weapon = weapon;
-  console.log("this:", this);
+// //constructor functions
+// function Elf(name, weapon) {
+//   console.log("this:", this);
+//   this.name = name;
+//   this.weapon = weapon;
+//   console.log("this:", this);
+// }
+// const josip = new Elf("josip", "stones");
+// console.log(josip);
+// Elf.prototype.attack = function attack() {
+//   return "attacks with " + this.weapon;
+// };
+// const check = new Elf("check", "fire");
+// console.log("check: ", check.attack());
+
+// const elf1 = new Function(
+//   "name",
+//   "weapon",
+//   `this.name = name;
+// this.weapon = weapon;`
+// );
+
+// const sarah = new elf1("sarah", "fire");
+// console.log(sarah);
+
+// var a = new Number(5);
+// console.log(typeof a);
+// var b = 5;
+// console.log(typeof b);
+// console.log(a === b);
+
+//OOP
+class Elf{
+  constructor(name, weapon){
+    this.name = name;
+    this.weapon = weapon;
+  }
+  attack(){
+    return 'attack with ' + this.weapon
+  }
 }
+
 const josip = new Elf("josip", "stones");
 console.log(josip);
-Elf.prototype.attack = function attack() {
-  return "attacks with " + this.weapon;
-};
-const check = new Elf("check", "fire");
-console.log("check: ", check.attack());
-
-const elf1 = new Function(
-  "name",
-  "weapon",
-  `this.name = name;
-this.weapon = weapon;`
-);
-
-const sarah = new elf1("sarah", "fire");
-console.log(sarah);
-
-var a = new Number(5);
-console.log(typeof a);
-var b = 5;
-console.log(typeof b);
-console.log(a === b);
+console.log(josip instanceof Elf)
