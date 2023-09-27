a5f754606d6a9f29245568e743501a7995edeb30
Cepa95 <ceprnic.josip@gmail.com>
2023-09-27 12:44:09 +0200

    async error handling

--- part08/app.js
+++ part08/app.js
@@ -4,13 +4,30 @@ function fail() {
   try {
     console.log("this works");
     // consol.log("this works");
-    throw new Error("oopsie")
+    throw new Error("oopsie");
   } catch (error) {
     console.log("we made an error: ", error);
-  }
-  finally{
+  } finally {
     console.log("still good");
   }
 }
 
 fail();
+//async error handling
+//.catch()
+Promise.resolve("asyncfail")
+  .then((response) => {
+    throw new Error("1 fail");
+    console.log(response);
+    return response;
+  })
+  .then((response) => {
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log(error);
+    return error;
+  })
+  .then((response) => {
+    console.log(response);
+  });
